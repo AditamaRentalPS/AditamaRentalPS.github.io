1ce20ad54b45946a03de3c1c7f0045949c8ba99b
Nawal Haromaen <nawalharomaen@gmail.com>
2024-11-21 17:32:40 +0700

    js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function scrollToSection(sectionId) {
-  const section = document.getElementById(sectionId);
+function scrollToSection(id) {
+  const section = document.getElementById(id);
   if (section) {
       section.scrollIntoView({ behavior: 'smooth' });
   }
 }
+
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href').substring(1);
+      scrollToSection(targetId);
+  });
+});
