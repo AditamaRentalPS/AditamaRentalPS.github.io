0afdbcf5ffa3a2107c0c93d7d98676f68fde3e08
Nawal Haromaen <nawalharomaen@gmail.com>
2024-11-21 17:04:59 +0700

    Edit

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-// Script to update the date dynamically
-document.addEventListener("DOMContentLoaded", () => {
-    const dateElement = document.getElementById("date");
-    const today = new Date();
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    dateElement.textContent = today.toLocaleDateString("en-US", options);
-  });
-  
+function scrollToSection(sectionId) {
+  const section = document.getElementById(sectionId);
+  if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+  }
+}
