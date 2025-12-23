890905478648c8ac7023b4aa17392da029b472ed
Hasan Zubeir Pohan <hasanpohan035@gmail.com>
2025-12-23 20:17:07 +0700

    save

--- admin/generate_admin_hash.php
+++
@@ -1,7 +0,0 @@
-<?php
-// generate_admin_hash.php
-$admin_password_mentah = "admin123"; // GANTI DENGAN PASSWORD ADMIN YANG KUAT DAN MUDAH ANDA INGAT!
-$hashed_password = password_hash($admin_password_mentah, PASSWORD_BCRYPT);
-echo "Password mentah: " . $admin_password_mentah . "<br>";
-echo "Hash password Anda adalah: <pre>" . $hashed_password . "</pre>";
-?>
