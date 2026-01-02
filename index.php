f6680f49c7df123cb4f4afb2472f822cb389d88a
Hasan Zubeir Pohan <hasanpohan035@gmail.com>
2026-01-03 03:40:57 +0700

    save

--- index.php
+++ index.php
@@ -27,7 +27,17 @@ $psPackages = [
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"/>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&amp;display=swap" rel="stylesheet"/>
-    <link href="assets/css/style.css" rel="stylesheet"/>
+    <style>
+        body {
+            font-family: 'Montserrat', sans-serif;
+        }
+        /* Style untuk menyesuaikan gambar pada bagian game slideshow */
+        .game-item img {
+            width: 100%;
+            height: 256px; /* Tinggi tetap */
+            object-fit: cover; /* Pastikan gambar memenuhi area tanpa terdistorsi */
+        }
+    </style>
 </head>
 <body class="bg-gray-900 text-white relative">
 
